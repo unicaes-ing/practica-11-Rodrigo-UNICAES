d3f9ea20cfba3e4c6fb8b33331216bafd607d932
Rodrigo-UNICAES <54501101+Rodrigo-UNICAES@users.noreply.github.com>
2019-10-30 17:08:49 -0600

    Add files via upload
    Aqui va el ejercicio 1 y 2

---
+++ ConsoleApp1/ConsoleApp1/Leer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Leer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream file = new FileStream("clientes.doc", FileMode.Open, FileAccess.Read);
+                BinaryReader wiwi = new BinaryReader(file);
+                string nombre = wiwi.ReadString();
+                int cel = wiwi.ReadInt32();
+                string date = wiwi.ReadString();
+                decimal sueldo = wiwi.ReadDecimal();
+                file.Close();
+                wiwi.Close();
+                Console.WriteLine("Datos del cliente: ");
+                Console.WriteLine();
+                Console.WriteLine("Nombre: {0}", nombre);
+                Console.WriteLine("Telefono: {0}", cel);
+                Console.WriteLine("Fecha de nacimiento: {0}", date);
+                Console.WriteLine("Salario: {0}$", sueldo);
+                Console.WriteLine();
+                Console.WriteLine("Presione culaquier tecla para salir.");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error bug 155555555 /)(/&(/&%/)(=/)%&%(7 ");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Nombre: ");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Telefono: ");
+                int cel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Fecha de nacimiento: ");
+                string date = Console.ReadLine();
+                Console.WriteLine("Salario: ");
+                decimal sueldo = decimal.Parse(Console.ReadLine());
+                FileStream file = new FileStream("clientes.doc", FileMode.Create, FileAccess.Write);
+                BinaryWriter wiwi = new BinaryWriter(file);
+                wiwi.Write(nombre);
+                wiwi.Write(cel);
+                wiwi.Write(date);
+                wiwi.Write(sueldo);
+                file.Close();
+                wiwi.Close();
+                Console.WriteLine();
+                Console.WriteLine("Datos Guardados........ ");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error en el proceso..... ");
+            }
+        }
+    }
+}
